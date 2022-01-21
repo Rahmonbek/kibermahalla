f84941d4cd318bf9e855f1c31b17f0e0f2f739c6
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2022-01-21 16:18:21 +0500

    cs;lmva

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Dashboard from './pages/Dashboard'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Dashboard/>
+      </div>
+    )
+  }
+}
